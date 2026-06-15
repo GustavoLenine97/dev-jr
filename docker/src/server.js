d99e8c41f911509d467accaea6e8fbe8d9aa0c5b
GustavoLenine97 <gustavolkjh_@hotmail.com>
2026-06-15 18:17:40 -0300

    dockermaisnode

--- docker/src/server.js
+++ docker/src/server.js
@@ -3,14 +3,18 @@ import userRoutes from './routes.js'
 import Sequelize  from 'sequelize'
 import config from './config/database.js'
 import User from './models/User.js'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
+
 
 const sequelize = new Sequelize(config)
 
 User.init(sequelize)
 app.use('/usuarios', userRoutes)
+//app.use('/usuarios/cadastro', userRoutes)
 
 sequelize.authenticate().then( () => {
     console.log("Banco de dados Conectado")
